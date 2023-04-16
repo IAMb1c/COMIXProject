5afd6aa26f9abcbede1a21253a19ef38da34de2a
Susan Subba <susansubba@susans-mbp.wireless.rit.edu>
2023-04-16 13:41:45 -0400

    undo and redo implemented

--- comicDatabaseController/ImportData.java
+++ comicDatabaseController/ImportData.java
@@ -1,53 +1,32 @@
 package comicDatabaseController;
-import java.util.HashMap;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.List;
-import java.util.Map;
-import java.util.Stack;
+
+import javax.smartcardio.CommandAPDU;
 
 /**
  * Parses csv for data 
  */
 public class ImportData implements ModifyCommand {
+    public List<ModifyCommand> commandsExecuted;
+    private final String id = "undo";
+    public List<Comic> database;
 
-    private Map<String, Comic> database = new HashMap<>();
-    
-    // public void read(){
-    //     try(BufferedReader reader = new BufferedReader(new FileReader("comics.csv"))){
-    //         int i  = 0;
-    //         int j = 0;
-    //         String line;
-    //         while((line = reader.readLine()) != null){
-    //             if(i > 3){
-    //                 String[] data = line.split(",");
-    //                 // for(String value:data){
-    //                 //     System.out.print(value);
-    //                 // }
-    //                 Comic comic = new Comic(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]);
-    //                 System.out.println(comic.getSeriesTitle());
-    //                 database.put(data[0], comic);
-    //             }
-    //             if(j == 10){break;}
-    //             i++;
-    //             j++;
-    //         }
-    //     }
-    //     catch (IOException e) {
-    //         e.printStackTrace();
-    //     }
-    // }
-    /**
-     * 
-     * @return Hashmap of String and Comic
-     * Name is the key and Comic object is the value
-     */
-    public Map<String, Comic> getDatabase(){
-        return this.database;
+    public ImportData(List<ModifyCommand> commandsExecuted, List<Comic> database){
+        this.commandsExecuted  = commandsExecuted;
+        this.database = database;
     }
 
     @Override
     public void executeCommand() {
-        // TODO Auto-generated method stub
         
+    }
+
+    @Override
+    public String getId() {
+        return null;
     }
     
 }
