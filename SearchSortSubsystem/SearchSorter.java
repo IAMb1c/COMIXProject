2f85bb5302a8af70dd39082e176b2c6dbe6cca3f
IAMb1c <shadowspades23@yahoo.com>
2023-04-17 15:59:52 -0400

    finished the searchsorter context class

--- SearchSortSubsystem/SearchSorter.java
+++ SearchSortSubsystem/SearchSorter.java
@@ -1,5 +1,9 @@
 package SearchSortSubsystem;
 import SearchSortSubsystem.ComicSort.*;
+import main.Comic;
+
+import java.util.ArrayList;
+
 import SearchSortSubsystem.ComicSearch.*;
 
 /** In the strategy pattern, this is the context: it has access to the separate searches and sorts. */
@@ -7,5 +11,17 @@ public class SearchSorter {
     private ComicSearch searcher;
     private ComicSort sorter;
 
-    
+    public void setSearch(ComicSearch newSearcher) {
+        this.searcher = newSearcher;
+    }
+    public void setSort(ComicSort newSorter) {
+        this.sorter = newSorter;
+    }
+
+    public ArrayList<Comic> searchComics(String term) {
+        return searcher.search(term);
+    }
+    public ArrayList<Comic> sortComics(ArrayList<Comic> comics) {
+        return sorter.sort(comics);
+    }
 }
